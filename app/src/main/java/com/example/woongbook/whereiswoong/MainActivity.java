f2725827f220ed1ca94bc57b08017bcb810d1659
nicewoong <thoughtscode@gmail.com>
2016-09-24 21:19:36 +0900

    feature/nicewoong : created project in default

---
+++ app/src/main/java/com/example/woongbook/whereiswoong/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.woongbook.whereiswoong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
