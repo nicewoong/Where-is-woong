275c3432b08eeeb7491eb1ceebb4f39ba1421d4f
nicewoong <thoughtscode@gmail.com>
2016-09-24 21:58:07 +0900

    - added google play service library to use LocationApi - Made MeetingLocation Class that work for finding location.

--- app/src/main/java/com/example/woongbook/whereiswoong/MainActivity.java
+++ app/src/main/java/com/example/woongbook/whereiswoong/MainActivity.java
@@ -1,28 +1,28 @@
 package com.example.woongbook.whereiswoong;
 
 import android.app.Activity;
-import android.content.DialogInterface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
-    private Button findingLocationButton ;
+    private Button saveLocationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findingLocationButton = (Button) findViewById(R.id.findingLocationButton);
-        findingLocationButton.setOnClickListener(this);
+
 
     }
 
     private void initUI(){
+        //init button setting
+        saveLocationButton = (Button) findViewById(R.id.findingLocationButton);
+        saveLocationButton.setOnClickListener(this);
+
     }
 
     @Override
@@ -33,6 +33,11 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.findingLocationButton:
 
+
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/woongbook/whereiswoong/MeetingLocation.java
@@ -0,0 +1,15 @@
+package com.example.woongbook.whereiswoong;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+
+/**
+ * Created by woongbook on 2016. 9. 24..
+ */
+public class MeetingLocation {
+    private GoogleApiClient mGoogleApiClient;
+
+    public void createGoogleApiClient(){
+
+    }
+
+}
